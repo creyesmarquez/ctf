a6d0492f95fc3e46251468033f0b52b4cd4841eb
Carlos Reyes Marquez <creyesmarquez@gmail.com>
2023-02-22 17:16:16 -0500

    Home page title

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,24 +3,23 @@ import { useTranslation } from "react-i18next";
 import { Row, Col } from "react-bootstrap";
 import "../styles/Home.css";
 
-import "../styles/Home.css";
-
 export default function Home() {
-  //const logo = require(`../../assets/images/sherbrooke-bg.jpeg`);
+  const logo = require(`../../assets/images/sherbrooke-bg.jpeg`);
   const { t } = useTranslation();
 
   return (
     <>
-      <Row className="d-grid justify-content-md-center mt-5" id="title">
-        <h1>
-          <strong>{t("welcome")}</strong>
-        </h1>
+      <Row>
+        <Row id="title" className="d-grid justify-content-md-center mt-5">
+          {t("welcome")}
+        </Row>
+        <Row className="" id="description" xs lg="2">
+          <Col>
+            <p>{t("weekly-reunion")}</p>
+          </Col>
+        </Row>
       </Row>
-      <Row className="" id="description" xs lg="2">
-        <Col>
-          <p>{t("welcome-description")}</p>
-        </Col>
-      </Row>
+      <img src={logo} className="img-fluid" alt="logo" />
     </>
   );
 }
