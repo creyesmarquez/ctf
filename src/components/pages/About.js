8646eaee4cb8ccbafbfb6b5083b931e92f2daf47
Carlos Reyes Marquez <creyesmarquez@gmail.com>
2023-02-28 00:06:13 -0500

    about images + font sizes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import "../styles/About.css";
 
 export default function About() {
   const { t } = useTranslation();
+  const logo = require(`../../assets/images/young-adults.jpeg`);
+  const logo2 = require(`../../assets/images/young-adults2.jpeg`);
+  const logo3 = require(`../../assets/images/young-adults3.jpeg`);
+  const ignitelogo = require(`../../assets/images/ignite-group.jpeg`);
   return (
     <>
       <Row
@@ -27,10 +31,38 @@ export default function About() {
         {t("young-adults-1")} <br />
         {t("young-adults-2")} <br />
         {t("young-adults-3")}
+        <Row className="mt-5">
+          <img
+            className="img-fluid"
+            style={{ maxWidth: "33%", padding: "0" }}
+            src={logo2}
+            alt="logo"
+          />
+          <img
+            className="img-fluid"
+            style={{ maxWidth: "33%", padding: "0" }}
+            src={logo}
+            alt="logo"
+          />
+          <img
+            className="img-fluid"
+            style={{ maxWidth: "33%", padding: "0" }}
+            src={logo3}
+            alt="logo"
+          />
+        </Row>
         <div id="services-subtitle">{t("ignite-group-title")}</div>
         {t("ignite-group-1")} <br />
         {t("ignite-group-2")} <br />
         {t("ignite-group-3")}
+        <Row className="mt-5">
+          <img
+            align="center"
+            style={{ width: "auto", padding: "0" }}
+            src={ignitelogo}
+            alt="logo"
+          />
+        </Row>
       </Row>
     </>
   );
