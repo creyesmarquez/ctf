1d6a482f4fbd87d4910eea72a2d01e52f3d74c71
Carlos Reyes Marquez <creyesmarquez@gmail.com>
2023-06-27 15:31:13 -0400

    about page changes + nort font

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import "../styles/About.css";
 
 export default function About() {
   const { t } = useTranslation();
-  const logo = require(`../../assets/images/young-adults.jpeg`);
-  const logo2 = require(`../../assets/images/young-adults2.jpeg`);
-  const logo3 = require(`../../assets/images/young-adults3.jpeg`);
+  const youngAdultsPicture = require(`../../assets/images/young-adults.jpeg`);
   const presencePicture = require(`../../assets/images/presence-schedule.jpg`);
   const youngAdultsDates = t("young-adults-4", { returnObjects: true });
 
@@ -29,46 +27,62 @@ export default function About() {
         <div id="services-subtitle"> {t("services-day")}</div>
         {t("services-day-subtitle")} <br />
         {t("services-day-info")}
+        <span
+          className="border-bottom"
+          style={{
+            marginTop: "2rem",
+          }}
+        ></span>
         <div id="services-subtitle">{t("young-adults-title")}</div>
-        {t("young-adults-1")} <br />
-        {t("young-adults-2")} <br />
-        {t("young-adults-3")}
-        <br />
-        <br />
-        {youngAdultsDates.map((date) => (
-          <div style={{ padding: "initial" }}>
-            {date} <br />
-          </div>
-        ))}
-        <Row className="mt-5">
-          <img
-            className="img-fluid"
-            style={{ maxWidth: "33%", padding: "0" }}
-            src={logo2}
-            alt="logo"
-          />
-          <img
-            className="img-fluid"
-            style={{ maxWidth: "33%", padding: "0" }}
-            src={logo}
-            alt="logo"
-          />
-          <img
-            className="img-fluid"
-            style={{ maxWidth: "33%", padding: "0" }}
-            src={logo3}
-            alt="logo"
-          />
+        <Row
+          style={{
+            padding: "initial",
+          }}
+        >
+          <Col>
+            {t("young-adults-1")} <br />
+            {t("young-adults-2")} <br />
+            {t("young-adults-3")}
+            <br />
+            <br />
+            <div id="young-adults-schedule">
+              {youngAdultsDates.map((date) => (
+                <div style={{ padding: "initial" }}>
+                  {date} <br />
+                </div>
+              ))}
+            </div>
+          </Col>
+          <Col>
+            <img
+              align="center"
+              style={{
+                maxWidth: "100%",
+                minWidth: "300px",
+                paddingLeft: "0",
+                paddingTop: "1rem",
+              }}
+              src={youngAdultsPicture}
+              alt="logo"
+            />
+          </Col>
         </Row>
-        <div id="services-subtitle">{t("ignite-group-title")}</div>
-        <Row className="">
-          <img
-            align="center"
-            style={{ width: "auto", padding: "0" }}
-            src={presencePicture}
-            alt="logo"
-          />
-        </Row>
+        <span
+          className="border-bottom"
+          style={{
+            marginTop: "2rem",
+          }}
+        ></span>
+        <div id="presence-container">
+          <Row>
+            <img
+              align="center"
+              style={{ width: "auto", padding: "0" }}
+              src={presencePicture}
+              alt="logo"
+            />
+          </Row>
+        </div>
       </Row>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./components/styles/index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
+
+import "./components/styles/index.css";
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
